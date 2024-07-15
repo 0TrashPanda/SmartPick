bfe52f7ff5c709f24f3f6588d79ca272f3770e99
Jonah-Van-den-Brandt <jonah.vandenbrandt@student.hogent.be>
2024-07-15 03:50:42 +0200

    first commit

---
+++ src/main/java/smart/pick/mixin/PickBlockMixin.java
@@ -0,0 +1,46 @@
+package smart.pick.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.List;
+
+import org.anti_ad.mc.ipn.api.access.IPN;
+
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
+
+@Mixin(PlayerInventory.class)
+public class PickBlockMixin {
+    @Inject(method = "getSwappableHotbarSlot()I", at = @At("HEAD"), cancellable = true)
+    public void SkipLockedSlots(CallbackInfoReturnable<Integer> ci) {
+        @SuppressWarnings("unchecked")
+        List<Integer> lockedSlots = IPN.getInstance().getLockedSlots();
+        List<Integer> hotbarSlots = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8);
+        // if all or none of the hotbar slots are locked
+        if (lockedSlots.containsAll(hotbarSlots) || lockedSlots.stream().anyMatch(hotbarSlots::contains)) {
+            ci.cancel();
+        }
+
+        int selectedSlot = ((PlayerInventory) (Object) this).selectedSlot;
+        DefaultedList<ItemStack> main = ((PlayerInventory) (Object) this).main;
+        for (int i = 0; i < 9; i++) {
+            int j = (selectedSlot + i) % 9;
+            if (main.get(j).isEmpty() && !lockedSlots.contains(j)) {
+                ci.setReturnValue(j);
+                return;
+            }
+        }
+
+        for (int ix = 0; ix < 9; ix++) {
+            int j = (selectedSlot + ix) % 9;
+            if (!main.get(j).hasEnchantments() && !lockedSlots.contains(j)) {
+                ci.setReturnValue(j);
+                return;
+            }
+        }
+    }
+}
